f7252966c3d410c22922297db0f7709dcd211598
nicolasrannou <npm.rannou@gmail.com>
2015-07-28 18:12:37 +0200

    update to latest master

--- examples/geometries_slice/geometries_slice.js
+++ examples/geometries_slice/geometries_slice.js
@@ -14,46 +14,62 @@
         /* globals Stats, dat*/
         "use strict";
 
-        var vjsSliceGeometry = require("../../src/geometries/geometries.slice");
-        var vjsOrbitControl2D = require("../../src/controls/OrbitControls2D");
+        var VJS = VJS || {};
+        VJS.geometries = VJS.geometries || {};
+        VJS.geometries.slice = require("../../src/geometries/geometries.slice");
+
+        VJS.controls = VJS.controls || {};
+        VJS.controls.orbitControls2D = require("../../src/controls/OrbitControls2D");
 
         // standard global variables
-        var controls, renderer, scene, camera, statsyay, particleLight, slice, arrowHelper;
-        var arrowUpdate = {
-            "direction": {
-                "i": 0,
-                "j": 0,
-                "k": 0
-            },
-            "position": {
-                "i": 0,
-                "j": 0,
-                "k": 0
-            }
-        };
-        var dimensions = new THREE.Vector3(123, 45, 67);
-        var halfDimensions = dimensions.clone().divideScalar(2);
-        var center = new THREE.Vector3(0, 0, 0);
-        var orientation = new THREE.Vector3(new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 1));
+        var controls, renderer, scene, camera, statsyay, particleLight, sliceMesh, arrowMesh;
 
-        // make the direction!!
-        var arrowLength = 20;
-        var arrowColor = 0xF44336;
+        var arrow = {
+            "direction": new THREE.Vector3(0, 0, 0),
+            "position": new THREE.Vector3(0, 0, 0),
+            "length": 20,
+            "color": 0xFFF336
+        };
+
+        var box = {
+            "dimensions": new THREE.Vector3(123, 45, 67),
+            "halfDimensions": new THREE.Vector3(123, 45, 67).divideScalar(2),
+            "center": new THREE.Vector3(0, 0, 0),
+            "orientation": new THREE.Vector3(new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 1))
+        };
+
+        function disposeSliceMesh() {
+            scene.remove(sliceMesh);
+            sliceMesh.geometry.dispose();
+            sliceMesh.material.dispose();
+            sliceMesh = null;
+        }
+
+        function createSliceMesh() {
+            // create the geometry
+            var sliceGeometry = new VJS.geometries.slice(box.halfDimensions, box.center, box.orientation, arrow.position, arrow.direction);
+            // create the material
+            var sliceMaterial = new THREE.MeshLambertMaterial({
+                color: 0x03A9F4,
+                emissive: 0x000000,
+                shininess: 30,
+                shading: THREE.SmoothShading,
+                "side": THREE.DoubleSide
+            });
+            return new THREE.Mesh(sliceGeometry, sliceMaterial);
+        }
 
         function updateGeometries() {
-            if (arrowHelper && slice) {
-                var newDirection = new THREE.Vector3(arrowUpdate.direction.i, arrowUpdate.direction.j, arrowUpdate.direction.k);
-                newDirection.normalize();
-                var newPosition = new THREE.Vector3(arrowUpdate.position.i, arrowUpdate.position.j, arrowUpdate.position.k);
+            if (arrowMesh && sliceMesh) {
+                // update arrow
+                arrowMesh.position.set(arrow.position.x, arrow.position.y, arrow.position.z);
+                arrowMesh.setDirection(arrow.direction);
 
-                scene.remove(arrowHelper);
-                arrowHelper = new THREE.ArrowHelper(newDirection, newPosition, arrowLength, arrowColor);
-                scene.add(arrowHelper);
-
-                // is memory leaking???
-                var newSliceGeometry = new vjsSliceGeometry(halfDimensions, center, orientation, newPosition, newDirection);
-                slice.geometry = newSliceGeometry;
-                slice.geometry.verticesNeedUpdate = true;
+                // create new slice
+                // not super efficient...
+                disposeSliceMesh();
+                sliceMesh = createSliceMesh();
+                scene.add(sliceMesh);
             }
         }
 
@@ -66,16 +82,15 @@
                 // update light position
                 var timer = Date.now() * 0.00025;
 
-                particleLight.position.x = Math.sin(timer * 7) * 80;
-                particleLight.position.y = Math.cos(timer * 5) * 90;
-                particleLight.position.z = Math.cos(timer * 3) * 100;
+                particleLight.position.x = Math.sin(timer * 7) * 70;
+                particleLight.position.y = Math.cos(timer * 5) * 80;
+                particleLight.position.z = Math.cos(timer * 3) * 90;
 
                 //update normal to look at particle
-                var newDirection = new THREE.Vector3(particleLight.position.x - arrowUpdate.position.i, particleLight.position.y - arrowUpdate.position.j, particleLight.position.z - arrowUpdate.position.k).normalize();
-
-                arrowUpdate.direction.i = newDirection.x;
-                arrowUpdate.direction.j = newDirection.y;
-                arrowUpdate.direction.k = newDirection.z;
+                var dir = new THREE.Vector3(particleLight.position.x - arrow.position.x, particleLight.position.y - arrow.position.y, particleLight.position.z - arrow.position.z).normalize();
+                arrow.direction.x = dir.x;
+                arrow.direction.y = dir.y;
+                arrow.direction.z = dir.z;
 
                 updateGeometries();
 
@@ -116,7 +131,7 @@
             camera.position.z = 100;
             camera.lookAt(scene.position);
             // controls
-            controls = new vjsOrbitControl2D(camera, renderer.domElement);
+            controls = new VJS.controls.orbitControls2D(camera, renderer.domElement);
 
             scene.add(new THREE.AmbientLight(0x444444));
 
@@ -124,7 +139,7 @@
             directionalLight.position.set(1, 1, 1).normalize();
             scene.add(directionalLight);
 
-            particleLight = new THREE.Mesh(new THREE.SphereGeometry(2, 8, 8), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+            particleLight = new THREE.Mesh(new THREE.SphereGeometry(2, 8, 8), new THREE.MeshBasicMaterial({ color: 0xFFF336 }));
             scene.add(particleLight);
 
             var pointLight = new THREE.PointLight(0xffffff, 2, 200);
@@ -138,51 +153,21 @@
             init();
 
             // make a box!
-
-            var boxGeometry = new THREE.BoxGeometry(dimensions.x, dimensions.y, dimensions.z);
+            var boxGeometry = new THREE.BoxGeometry(box.dimensions.x, box.dimensions.y, box.dimensions.z);
             var boxMaterial = new THREE.MeshBasicMaterial({
                 wireframe: true,
                 color: 0x61F2F3
             });
-            var box = new THREE.Mesh(boxGeometry, boxMaterial);
-            scene.add(box);
+            var boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+            scene.add(boxMesh);
 
             // make a slice!
-            var position = center.clone();
-            var direction = new THREE.Vector3(-0.2, 0.5, 0.3);
+            sliceMesh = createSliceMesh();
+            scene.add(sliceMesh);
 
-            var sliceGeometry = new vjsSliceGeometry(halfDimensions, center, orientation, position, direction);
-            // var shininess = 50, specular = 0x333333, bumpScale = 1, shading = THREE.SmoothShading;
-            // new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading }
-            var sliceMaterial = new THREE.MeshLambertMaterial({
-                color: 0x03A9F4,
-                emissive: 0x000000,
-                shininess: 30,
-                shading: THREE.SmoothShading,
-                "side": THREE.DoubleSide
-            });
-            // new THREE.MeshBasicMaterial({
-            //   'side': THREE.DoubleSide,
-            //   'color': 0x03A9F4
-            // });
-            slice = new THREE.Mesh(sliceGeometry, sliceMaterial);
-            scene.add(slice);
-
-            arrowHelper = new THREE.ArrowHelper(direction, position, arrowLength, arrowColor);
-            arrowUpdate = {
-                "direction": {
-                    "i": direction.x,
-                    "j": direction.y,
-                    "k": direction.z
-                },
-                "position": {
-                    "i": position.x,
-                    "j": position.y,
-                    "k": position.z
-                }
-            };
-
-            scene.add(arrowHelper);
+            // make an arrow
+            arrowMesh = new THREE.ArrowHelper(arrow.direction, arrow.position, arrow.length, arrow.color);
+            scene.add(arrowMesh);
 
             var gui = new dat.GUI({
                 autoPlace: false
@@ -192,40 +177,23 @@
             customContainer.appendChild(gui.domElement);
 
             var directionFolder = gui.addFolder("Plane direction");
-            var frameIndexControllerDirectionI = directionFolder.add(arrowUpdate.direction, "i", -1, 1).listen();
-            var frameIndexControllerDirectionJ = directionFolder.add(arrowUpdate.direction, "j", -1, 1).listen();
-            var frameIndexControllerDirectionK = directionFolder.add(arrowUpdate.direction, "k", -1, 1).listen();
+            var frameIndexControllerDirectionI = directionFolder.add(arrow.direction, "x", -1, 1).listen();
+            var frameIndexControllerDirectionJ = directionFolder.add(arrow.direction, "y", -1, 1).listen();
+            var frameIndexControllerDirectionK = directionFolder.add(arrow.direction, "z", -1, 1).listen();
             directionFolder.open();
 
             var positionFolder = gui.addFolder("Plane position");
-            var frameIndexControllerOriginI = positionFolder.add(arrowUpdate.position, "i", -61.5, 61.5).listen();
-            var frameIndexControllerOriginJ = positionFolder.add(arrowUpdate.position, "j", -22.5, 22.5).listen();
-            var frameIndexControllerOriginK = positionFolder.add(arrowUpdate.position, "k", -33.5, 33.5).listen();
+            var frameIndexControllerOriginI = positionFolder.add(arrow.position, "x", -61.5, 61.5).listen();
+            var frameIndexControllerOriginJ = positionFolder.add(arrow.position, "y", -22.5, 22.5).listen();
+            var frameIndexControllerOriginK = positionFolder.add(arrow.position, "z", -33.5, 33.5).listen();
             positionFolder.open();
 
-            frameIndexControllerDirectionI.onChange(function () {
-                updateGeometries();
-            });
-
-            frameIndexControllerDirectionJ.onChange(function () {
-                updateGeometries();
-            });
-
-            frameIndexControllerDirectionK.onChange(function () {
-                updateGeometries();
-            });
-
-            frameIndexControllerOriginI.onChange(function () {
-                updateGeometries();
-            });
-
-            frameIndexControllerOriginJ.onChange(function () {
-                updateGeometries();
-            });
-
-            frameIndexControllerOriginK.onChange(function () {
-                updateGeometries();
-            });
+            frameIndexControllerDirectionI.onChange(updateGeometries);
+            frameIndexControllerDirectionJ.onChange(updateGeometries);
+            frameIndexControllerDirectionK.onChange(updateGeometries);
+            frameIndexControllerOriginI.onChange(updateGeometries);
+            frameIndexControllerOriginJ.onChange(updateGeometries);
+            frameIndexControllerOriginK.onChange(updateGeometries);
         };
     }, { "../../src/controls/OrbitControls2D": 2, "../../src/geometries/geometries.slice": 4 }], 2: [function (require, module, exports) {
         "use strict";
